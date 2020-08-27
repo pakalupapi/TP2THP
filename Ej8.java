75ecca4133118885604c58ca5edadd89483626b8
pakalupapi <osvaldolucero98@gmail.com>
2020-08-27 13:22:11 -0300

    same sin scanner

--- Ej8.java
+++ Ej8.java
@@ -1,10 +1,25 @@
 package ejercicios;
 
 public class Ej8 {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num1=5;
+		int num2=3;
+		System.out.println(esMultiplo(num1, num2));
+		
+		num1=15;
+		num2=3;
+		System.out.println(esMultiplo(num1, num2));
 
 	}
-
+	public static boolean esMultiplo(int num1, int num2) {
+		int resto;
+		boolean esMultiplo;
+		
+		resto=num1%num2;
+		esMultiplo=resto==0;
+		
+		return esMultiplo;
+	}
 }

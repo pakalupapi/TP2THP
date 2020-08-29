1296135acf94a339292750abfe64916331bea941
pakalupapi <osvaldolucero98@gmail.com>
2020-08-29 16:42:01 -0300

    no inicializa a

--- Ej15.java
+++ Ej15.java
@@ -7,12 +7,10 @@ public class Ej15 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int numeroCliente;
 		Double cuenta=0.0;
 		Double duracionLlamada;
-		Double duracion[]=new Double[2];
+		Double duracion[]=new Double[2], recaudacionABC[]=new Double[3];
 		Double importePorLlamada, importeTotal=0.0, precioPromedio=0.0;
-		Double aRecaudarA=0.0, aRecaudarB=0.0, aRecaudarC=0.0;
 		Double llamadaMasLarga=0.0, llamadaMasCorta=0.0;
 		String tipoDeAbono="";
 		boolean salir;
@@ -20,33 +18,24 @@ public class Ej15 {
 		duracion[0]=DURACION_MAX;
 		duracion[1]=DURACION_MIN;
 		do {
-		numeroCliente=bienvenida(cuenta);
+		bienvenida(cuenta);
 		duracionLlamada=obtenerDuracion();
-		duracion[0]=duracionMinMax(duracionLlamada, duracion[0], "min");
-		duracion[1]=duracionMinMax(duracionLlamada, duracion[1], "max");
+		duracion=duracionMinMax(duracionLlamada, duracion[0], duracion[1]);
 		tipoDeAbono=obtenerAbono();
 		importePorLlamada=obtenerFactura(tipoDeAbono, duracionLlamada);
 		importeTotal+=importePorLlamada;
 		cuenta+=1;
-		if(tipoDeAbono.equals("a")) {
-			aRecaudarA+=importePorLlamada;
-		}
-		else if(tipoDeAbono.equals("b")) {
-			aRecaudarB+=importePorLlamada;
-		}
-		else{
-			aRecaudarC+=importePorLlamada;
-		}
+		recaudacionABC=recaudacionManager(tipoDeAbono, importePorLlamada, recaudacionABC[0], recaudacionABC[1], recaudacionABC[2]);
 		salir=funcionSalida();
 		}while(!salir);
 		
 		llamadaMasLarga=duracion[1];
 		llamadaMasCorta=duracion[0];
 		precioPromedio=pedirPromedio(importeTotal, cuenta);
-		despedida(aRecaudarA, aRecaudarB, aRecaudarC, importeTotal, llamadaMasLarga, llamadaMasCorta, precioPromedio);
+		despedida(recaudacionABC[0], recaudacionABC[1], recaudacionABC[2], importeTotal, llamadaMasLarga, llamadaMasCorta, precioPromedio);
 		input.close();
 	}
-	public static int bienvenida(Double cuenta) {
+	public static void bienvenida(Double cuenta) {
 		int numeroCliente=0;
 		boolean numeroValido;
 		
@@ -58,7 +47,6 @@ public class Ej15 {
 			numeroCliente=Integer.parseInt(input.nextLine());
 			numeroValido=numeroCliente>=MINIMO_NUM_CLIENTE && numeroCliente<=MAXIMO_NUM_CLIENTE;
 		}while(!numeroValido);
-		return numeroCliente;
 	}
 	public static Double obtenerDuracion() {
 		Double duracion;
@@ -83,7 +71,6 @@ public class Ej15 {
 	}
 	public static Double obtenerFactura(String tipoDeAbono, Double duracionLlamada) {
 		Double factura=0.0;
-		Double precioMinuto=0.0;
 		final Double PRECIO_MIN_A=2.0;
 		final Double PRECIO_MIN_B=2.0;
 		final Double PRECIO_MIN_C=1.0;
@@ -91,15 +78,15 @@ public class Ej15 {
 		final Double PRECIO_PROMO_B=1.5, PRECIO_MAX_C=10.0;
 		
 		if (tipoDeAbono.equals("a")) {
-			factura=precioMinuto*duracionLlamada;
+			factura=PRECIO_MIN_A*duracionLlamada;
 		}
 		else if(tipoDeAbono.equals("b")) {
 			if (duracionLlamada>CINCO_MINS_B) {
-				factura=precioMinuto*CINCO_MINS_B;
+				factura=PRECIO_MIN_B*CINCO_MINS_B;
 				factura+=PRECIO_PROMO_B*(duracionLlamada-CINCO_MINS_B);
 			}
 			else if(duracionLlamada<CINCO_MINS_B) {
-				factura=precioMinuto*duracionLlamada;
+				factura=PRECIO_MIN_B*duracionLlamada;
 			}
 		}
 		else if(tipoDeAbono.equals("c")) {
@@ -107,10 +94,28 @@ public class Ej15 {
 				factura=PRECIO_MAX_C;
 			}
 			else {
-				factura=duracionLlamada*precioMinuto;
+				factura=duracionLlamada*PRECIO_MIN_C;
 			}
 		}
 		return factura;
+	}
+	public static Double[] recaudacionManager(String tipoDeAbono, Double importePorLlamada, Double recaudacionA, Double recaudacionB, Double recaudacionC) {
+		Double resultado[]=new Double[3];
+		
+		resultado[0]=recaudacionA;
+		resultado[1]=recaudacionB;
+		resultado[2]=recaudacionC;
+		
+		if(tipoDeAbono.equals("a")) {
+			resultado[0]+=importePorLlamada;
+		}
+		else if(tipoDeAbono.equals("b")) {
+			resultado[1]+=importePorLlamada;
+		}
+		else{
+			resultado[2]+=importePorLlamada;
+		}
+		return resultado;
 	}
 	public static boolean funcionSalida() {
 		String salida;
@@ -122,33 +127,20 @@ public class Ej15 {
 		
 		return quiereSalir;
 	}
-	public static Double duracionMinMax(Double duracionLlamada, Double duracionMinMax, String eleccion) {
-		Double duracionMax, duracionMin;
+	public static Double[] duracionMinMax(Double duracionLlamada, Double duracionMin, Double duracionMax) {
 		Double min_y_max[]=new Double[2];
-		int resultado=0;
 		
-		if(eleccion.equals("min")) {
-			if(duracionLlamada<duracionMinMax) {
-				duracionMin=duracionLlamada;
-			}
-			else {
-				duracionMin=duracionMinMax;
-			}
-			min_y_max[0]=duracionMin;
-			resultado=0;
+		min_y_max[0]=duracionMin;
+		min_y_max[1]=duracionMax;
+		
+		if(duracionLlamada<duracionMin) {
+			min_y_max[0]=duracionLlamada;
 		}
-		else if(eleccion.contentEquals("max")){
-			if (duracionLlamada>duracionMinMax) {
-				duracionMax=duracionLlamada;
-			}
-			else {
-				duracionMax=duracionMinMax;
-			}
-			min_y_max[1]=duracionMax;
-			resultado=1;
+		else if (duracionLlamada>duracionMax) {
+			min_y_max[1]=duracionLlamada;
 		}
 		
-		return min_y_max[resultado];
+		return min_y_max;
 	}
 	public static Double pedirPromedio(Double importeTotal, Double cantidadDeLlamadas) {
 		Double precioPromedio;
